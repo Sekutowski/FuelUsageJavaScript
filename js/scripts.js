b6c264152111bd0728cab052605a1baab808b049
Sekutowski <mobile.sekut@gmail.com>
2019-04-28 20:19:34 +0200

    upCode

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,34 @@
 var a = document.getElementById('firstValue');
 var b = document.getElementById('secondValue');
+var c = document.getElementById('thirdValue');
 
 
 function Calculate()
 {
-	var aa = Number(a.value);
+	var aa = parseFloat(a.value);
 		console.log(aa);
 	var bb = Number(b.value);
 		console.log(bb);
+	var cc = Number(c.value);
+		console.log(cc);
 		if(aa == 0 || bb == 0){
 			document.getElementById("score").innerHTML="Źle wprowadzono dane";
 		} else{
-			var x = Math.round((aa/bb)*100);
+			var x = (Math.round((aa/bb)*100)).toFixed(2);
 				console.log(x);
-				document.getElementById("score").innerHTML="<strong>Twoje spalanie wynośi: </strong><br />"+ x + " l/km";
-					if (x > 20){
-						LargeCombustion();
-					}
-		}
+			var y = (Math.round(cc*x).toFixed(2));
+				console.log(y);
+					if (y > 0){
+							document.getElementById("score").innerHTML="<strong>Twoje spalanie wynośi: </strong><br />"+ x + " l/km" + "<br /><br /> <strong>Koszt przejechania 100km wynosi:</strong> " + y + "zł";
+						} else{
+								document.getElementById("score").innerHTML="<strong>Twoje spalanie wynośi: </strong><br />"+ x + " l/km" ;
+							}
+			}
+		
 }
 
 function LargeCombustion()
 {
 	alert("Oho Twoja fura sporo pali ;)");
 }
+
